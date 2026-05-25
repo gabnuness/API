4ef54bb982854b648773c4d70800a3677c83a167
Gabriel <gabnunes.developer@gmail.com>
2026-05-25 19:27:55 -0300

    Adicionando os métodos PUT e DELETE para editar e deletar usuários

--- server.js
+++ server.js
@@ -1,25 +1,70 @@
 import express from "express"
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
 
 const app = express()
 app.use(express.json())
 
-const users = []
+app.post("/usuarios", async (req, res) => {
 
-app.post("/usuarios", (req, res) => {
-    
-    users.push(req.body)
+    await prisma.user.create({
+        data: {
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age
+        }
+    })
 
-    res.status(201).json(users)
-
+    res.status(201).json(req.body)
 })
 
-app.get("/usuarios", (req, res) => {
-  
+app.get("/usuarios", async (req, res) => {
+    let users = []
+
+    if (req.query){
+        users = await prisma.user.findMany({
+            where: {
+                name: req.query.name
+            }
+        })
+    }
+    else {
+        users = await prisma.user.findMany()
+    }
     res.status(200).json(users)
 })
 
+app.put("/usuarios/:id", async (req, res) => {
+
+     await prisma.user.update({
+            where: {
+                id: req.params.id 
+            },
+            data: {
+                email: req.body.email,
+                name: req.body.name,
+                age: req.body.age
+            }
+        })
+
+        res.status(201).json(req.body)
+        
+    })
+
+app.delete("/usuarios/:id", async (req, res) => {
+    await prisma.user.delete({
+        where: {
+            id: req.params.id
+        }
+    })
+
+    res.status(200).json({ message: " Usuário deletado com sucesso!"})
+})
+
+
 app.listen(3000, () => {
-  console.log("Rodando na porta 3000")
+    console.log("Rodando na porta 3000")
 })
 
 /* Criar API de usuários:
